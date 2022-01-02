008547a07b7315a84394ce1cae157039fc21576a
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-01-02 09:37:42 +0100

    now can add new posts.

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const {body, validationResult} = require('express-validator');
 
 exports.post_all_get = (req, res) => {
     Post.find()
@@ -7,10 +8,29 @@ exports.post_all_get = (req, res) => {
 };
 
 exports.post_new_get = (req, res) => {
-    res.json(
-        {
-            message: 'new post get request'
-        }
-    )
+    res.json({ message: 'new post get request' });
 };
 
+exports.post_new_post = [
+    body('text', 'Post should not be empty').trim().isLength({min:1}).escape(),
+
+    (req, res) => {
+        const errors = validationResult(req);
+
+        const post = new Post({
+            //need to change to get user id from req object
+            author: '61d159657ab36e7f277ee8d1',
+            text: req.body.text,
+            date: Date.now()
+        })
+
+        if (!errors.isEmpty()) {
+            res.json({ errors })
+        } else {
+            post.save()
+            //make it redirect somewhere
+            .then(() => res.json({ message: 'success' }))
+            .catch(err => res.json({err}))
+        }
+    }
+]
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const post_controller = require('../controllers/postsController');
 
 router.get('/', post_controller.post_all_get);
+router.post('/', post_controller.post_new_post);
 
 module.exports = router
